b05f6655a7ca0dc37d004d7c0b9aed807b77b3af
Luke Asperger <lukeasperger@MacBook-Pro.local>
2019-08-25 15:25:42 -0700

    finished solving

--- solver.py
+++ solver.py
@@ -1,16 +1,14 @@
 import numpy as np
-import torch
 import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
 import string
+import json
 
 def segment_maze(maze_height, maze_width):
     im = Image.open("maze.png")
-    im = im.resize((maze_width*224, maze_height*224))
-    im_arr = np.array(im)
-    height = im_arr.shape[0]
-    width = im_arr.shape[1]
+    im_arr = np.array(im.resize((maze_width*224, maze_height*224)))
+    height, width, _ = im_arr.shape
 
     frame_h = height//maze_height
     frame_w = width//maze_width
@@ -49,7 +47,6 @@ def label_images(grid, maze_height, maze_width, imagenet_dict):
         for j in range(maze_width):
             img = grid[i, j]
             img = Image.fromarray(img[:, :, :3])
-            trans = transforms.ToTensor()
             img_tens = transform(img)
             img_tens = img_tens.unsqueeze(0)
             scores = resnet18.forward(img_tens)
@@ -57,24 +54,69 @@ def label_images(grid, maze_height, maze_width, imagenet_dict):
             labels[i].append(imagenet_dict[score_arr.argmax()])
 
             # compare triceratops score to sports car score
-            dyno_score = np.average(score_arr[0, 38:52]) # looks at all types of lizards / dinosaurs
-            car_score = np.average(score_arr[0, 817], score_arr[0,864], score_arr[0,829], score_arr[0,751],
-                                   score_arr[0, 436], score_arr[0,479])
+            dyno_score = np.max([score_arr[0, 46], score_arr[0, 51]]) # looks at various lizards / dinosaurs
+            car_score = np.max([score_arr[0, 817], score_arr[0,864]]) # looks at various car types
             if car_score > dyno_score:
                 car_grid[i, j] = 1
 
     return car_grid
 
+def try_step(next_coord, labeled_grid, ending_coord, path):
+    maze_height, maze_width = labeled_grid.shape
+    if next_coord[0] < maze_height and next_coord[1] < maze_width and next_coord[0] >= 0 and next_coord[1] >= 0 and \
+            next_coord not in path:
+        if labeled_grid[next_coord]:
+            path.append(next_coord)
+        else:
+            return path, 0
+    else:
+        return path, 0
 
+    # base case, return 1 to signify success
+    if (next_coord == ending_coord):
+        return path, 1
+
+    left_path, success = try_step((next_coord[0], next_coord[1] - 1), labeled_grid, ending_coord, path.copy())
+    if success:
+        return left_path, 1
+
+    right_path, success = try_step((next_coord[0], next_coord[1] + 1), labeled_grid, ending_coord, path.copy())
+    if success:
+        return right_path, 1
+
+    up_path, success = try_step((next_coord[0] - 1, next_coord[1]), labeled_grid, ending_coord, path.copy())
+    if success:
+        return up_path, 1
+
+    down_path, success = try_step((next_coord[0] + 1, next_coord[1]), labeled_grid, ending_coord, path.copy())
+    if success:
+        return down_path, 1
+
+    return path, 0
+
+def find_path(labeled_grid, starting_coord, ending_coord):
+    path = []
+    path, _ = try_step(starting_coord, labeled_grid, ending_coord, path.copy())
+    return path
 
 # Set maze parameters here
 maze_height = 8
 maze_width = 14
+starting_coord = (3, 13)
+ending_coord = (3, 0)
 
 # Read image into maze
-grid = segment_maze(maze_height, maze_width)
-imagenet_dict = read_labels()
-labeled_grid = label_images(grid, maze_height, maze_width, imagenet_dict)
+# grid = segment_maze(maze_height, maze_width)
+# imagenet_dict = read_labels()
+# labeled_grid = label_images(grid, maze_height, maze_width, imagenet_dict)
+#
+# grid = labeled_grid.tolist()
+# with open('grid.txt', 'w') as outfile:
+#     json.dump(grid, outfile)
 
-# Solvd maze
+with open('grid.txt') as json_file:
+    labeled_grid = np.array(json.load(json_file))
+
+# Solve maze
+path = find_path(labeled_grid, starting_coord, ending_coord)
 print("done")
